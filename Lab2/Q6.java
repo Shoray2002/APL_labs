d3dbf6cda08dc0b9c9b00ae3308350f2ac53627f
Shoray Singhal <lci2020037@iiitl.ac.in>
2021-08-16 12:04:56 +0530

    Q6 done

--- Lab2/Q6.java
+++ Lab2/Q6.java
@@ -1,7 +1,7 @@
 package Lab2;
 public class Q6 {
     public static void main(String []args){
-         System.out.println("The first 50 Pentagonal Numbers are : \n");
+         System.out.println("The first 50 Pentagonal Numbers are :");
         for(int i=1;i<51;i++){
             System.out.print((3*i*i-i)/2);
             System.out.print("\n");
