41dbd55572bec85c722757b432b10132ac569f9d
Shoray Singhal <lci2020037@iiitl.ac.in>
2021-08-16 12:06:25 +0530

    Q8 done

--- Lab2/Q8.java
+++ Lab2/Q8.java
@@ -1,8 +1,8 @@
 package Lab2;
 public class Q8 {
-    // Write a Java program to print the Fibonacci series of the number up to 100
+
     public static void main(String[] args) {
-        System.out.println("Fibonacci series up to 100 :");
+        System.out.println("Fibonacci Series up to 100 is :");
         int x = 1, y = 1;
         while (x < 100) {
             System.out.println(x);
