056d6a13a8764d8699bf08caa3537b153da3d9e0
Shoray Singhal <lci2020037@iiitl.ac.in>
2021-08-16 10:50:56 +0530

    Q3done

---
+++ Lab2/Q3.java
@@ -0,0 +1,32 @@
+package Lab2;
+
+public class Q3 {
+    public static void main(String[] args) {
+        var m1 = new Mobile(30000, "Sony", "Shoray Singhal");
+        var m2 = new Mobile(60000, "Zeiss", "Kirti Singhal");
+        var m3 = new Mobile(95000, "Apple", "Priyanshu Upadhyay");
+
+        m1.print();
+        m2.print();
+        m3.print();
+    }
+}
+
+
+public class Mobile {
+    private int cost;
+    private String camera;
+    public String username;
+
+    public Mobile(int cost, String camera, String username) {
+        this.cost = cost;
+        this.camera = camera;
+        this.username = username;
+    }
+
+    public void print() {
+        System.out.println("Mobile cost: " + cost + "\nCamera: " + camera + "\nUsername: " + username);
+        System.out.println("------------------------------------------------------------");
+    }
+    
+}
