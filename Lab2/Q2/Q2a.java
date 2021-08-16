3bdc7d9d1e0ebb1a105f82ab364c2d463589f7db
Shoray Singhal <lci2020037@iiitl.ac.in>
2021-08-16 12:09:38 +0530

    Q2 done

--- Lab2/Q2/Q2a.java
+++ Lab2/Q2/Q2a.java
@@ -4,8 +4,8 @@ package Lab2.Q2;
 public class Q2a {
     public static void main(String []args){
          writeDays w=new writeDays();
-         w.setDay(30);
-         System.out.println("Number of days in august is : "+w.getDay());
+         w.setDay(31);
+         System.out.println("Number of Days in August is : "+w.getDay());
     }
 }
 
--- Lab2/Q2/Q2b.java
+++ Lab2/Q2/Q2b.java
@@ -3,11 +3,11 @@ package Lab2.Q2;
 public class Q2b {
     public static void main(String []args){
         days w=new days();
-        System.out.println("The number of days in august is: "+w.getDays());
+        System.out.println("Number of Days in August is : "+w.getDays());
     }
 }
 class days{
-    private int days=30;
+    private int days=31;
     public int getDays(){
         return days;
     }
