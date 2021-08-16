d4db2d041d94a227fb5f1b991089662a9e27386d
Shoray Singhal <lci2020037@iiitl.ac.in>
2021-08-16 12:11:05 +0530

    Q7 done

--- Lab2/Q7/Q7a.java
+++ Lab2/Q7/Q7a.java
@@ -12,7 +12,7 @@ public class Q7a {
     }
 
     public static void main(String[] args) {
-        System.out.println("**** Using Constructor Overloading ****");
+        // System.out.println("**** Using Constructor Overloading ****");
         System.out.println("Multiplying 4 and 5 : " + multiply(4, 5));
         System.out.println("Multiplying 2.36, 3.98, and 4 : " + multiply(2.36, 3.98, 4));
         System.out.println("Multiplying 2.68 and 3.5 : " + multiply(2.68, 3.5));
