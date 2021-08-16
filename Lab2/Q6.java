407b027cec4ac4b325faead9cbea5412dfe45706
Shoray Singhal <lci2020037@iiitl.ac.in>
2021-08-16 11:32:50 +0530

    Q6 done

---
+++ Lab2/Q6.java
@@ -0,0 +1,10 @@
+package Lab2;
+public class Q6 {
+    public static void main(String []args){
+         System.out.println("The first 50 Pentagonal Numbers are : \n");
+        for(int i=1;i<51;i++){
+            System.out.print((3*i*i-i)/2);
+            System.out.print("\n");
+        }
+    }
+}
